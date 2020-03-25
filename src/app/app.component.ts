067ebb11e879a89f580fa6a2f783c772181acb13
Willy De Keyser <willy.de.keyser@skynet.be>
2020-03-25 16:40:11 +0100

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'directives';
+  @HostListener("mouseleave", ["$event.target"])
+  onClick(btn) {
+    console.log("TEST HostListener " + btn.nativeElement);
+  }
+
+  title = "directives";
 }
